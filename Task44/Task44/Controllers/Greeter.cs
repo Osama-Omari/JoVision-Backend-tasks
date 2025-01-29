687d1d55c22b92b7906f2b0d73419ec0355d3baf
Osama-Omari <ORZOmari2014@gmail.com>
2025-01-29 22:43:21 +0300

    Task44

---
+++ Task44/Task44/Controllers/Greeter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task44.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Greeter : Controller
+    {
+        [HttpGet]
+        public IActionResult Greet([FromQuery] string name="anonymous")
+        {
+            return Ok($"Hello {name}");
+        }
+    }
+}
