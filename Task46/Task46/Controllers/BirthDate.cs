df856c27ec9138234bc3edf0ab7eb580c11488ce
Osama-Omari <ORZOmari2014@gmail.com>
2025-02-18 21:15:07 +0300

    .

---
+++ Task46/Task46/Controllers/BirthDate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task46.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BirthDate : Controller
+    {
+        public IActionResult Post([FromForm] string? name, [FromForm] int? years, [FromForm] int? months, [FromForm] int? days)
+        {
+            name = string.IsNullOrWhiteSpace(name) ? "Anonymous" : name;
+
+            if (years == null || months == null || days == null)
+            {
+                return Ok($"Hello {name}, I can’t calculate your age without knowing your birthdate!");
+            }
+
+            try
+            {
+                DateTime birthDate = new DateTime(years.Value, months.Value, days.Value);
+                int age = DateTime.Now.Year - birthDate.Year;
+                if (birthDate > DateTime.Now.AddYears(-age))
+                {
+                    age--;
+                }
+
+                return Ok($"Hello {name}, your age is {age}.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Invalid date input:{ex}");
+            }
+        }
+    }
+}
---
+++ Task46/Task46/Controllers/Greeter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task46.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Greeter : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($"Hello, Welcome to my API");
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromForm] string? name)
+        {
+            name = string.IsNullOrWhiteSpace(name) ? "Anonymous" : name;
+            return Ok($"Hello {name}, Welcome to my API");
+        }
+    }
+}
