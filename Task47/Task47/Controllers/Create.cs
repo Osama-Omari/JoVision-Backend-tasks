bd26a8d505bf2fc46c262e43915496bb20f50b58
Osama-Omari <ORZOmari2014@gmail.com>
2025-02-19 15:42:42 +0300

    .

---
+++ Task47/Task47/Controllers/Create.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Text.Json;
+namespace Task47.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Create : Controller
+    {
+
+        private readonly string storage = Path.Combine(Directory.GetCurrentDirectory(), "Images");
+            
+        public Create()
+        {
+            if (!Directory.Exists(storage))
+                Directory.CreateDirectory(storage);
+        }
+
+
+        [HttpPost("upload")]
+        public async  Task<IActionResult> UploadImage([FromForm]IFormFile image ,[FromForm]string owner )
+        {
+            if (image.Length == 0 || image == null || string.IsNullOrWhiteSpace(owner))
+                return BadRequest("Image and owner are required");
+
+            if (Path.GetExtension(image.FileName)?.ToLower() != ".jpg")
+                return BadRequest("Only jpg format is allowed");
+
+            string imagepath = Path.Combine(storage, image.FileName);
+            string imagemetadatapath = Path.Combine(storage, image.FileName + ".json");
+
+            if (System.IO.File.Exists(imagepath))
+                return BadRequest("This image is already exist");
+
+            try
+            {
+                using (var stream = new FileStream(imagepath, FileMode.Create))
+                {
+                    await image.CopyToAsync(stream);
+                }
+
+                var metadata = new
+                {
+                    OwnerName = owner,
+                    CreatedAt = DateTime.Now,
+                    LastModifiedAt = DateTime.Now
+                };
+                
+                await System.IO.File.WriteAllTextAsync(imagemetadatapath, JsonSerializer.Serialize(metadata));
+
+                return Ok("Image uploaded successfully.");
+
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Task47/Task47/Controllers/Delete.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Task47.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Delete : Controller
+    {
+        private readonly string storage = Path.Combine(Directory.GetCurrentDirectory(), "Images");
+
+        public class ImageMetadata
+        {
+            public string OwnerName { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public DateTime LastModifiedAt { get; set; }
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult delete([FromQuery] string FileName , string FileOwner)
+        {
+            if (string.IsNullOrWhiteSpace(FileName) || string.IsNullOrWhiteSpace(FileOwner))
+                return BadRequest("Bad request. File name and  File owner required");
+
+            string imagepath = Path.Combine(storage, FileName);
+            string imagemetadatapath = Path.Combine(storage, FileName + ".json");
+
+            if (!System.IO.File.Exists(imagepath))
+                return NotFound("image not found");
+            if (System.IO.File.Exists(imagemetadatapath))
+            {
+                string metadataJson = System.IO.File.ReadAllText(imagemetadatapath);
+                var metadata = JsonSerializer.Deserialize<ImageMetadata>(metadataJson);
+                if (string.IsNullOrEmpty(metadata?.OwnerName) ||
+                    !metadata.OwnerName.Trim().ToLower().Equals(FileOwner.Trim().ToLower()))
+                {
+                    Console.WriteLine($"File Owner: {FileOwner}");
+                    Console.WriteLine($"Metadata Owner: {metadata?.OwnerName}");
+                    return StatusCode(403, "The owner's name does not match.");
+                }
+            }
+            else
+            {
+                return StatusCode(403, "The Metadata for the file not found");
+            }
+
+            try
+            {
+                System.IO.File.Delete(imagepath);
+                System.IO.File.Delete(imagemetadatapath);
+
+                return Ok("Deleted");
+            }catch(Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
